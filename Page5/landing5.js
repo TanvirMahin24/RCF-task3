e2e2a461e17301301bfa2e5cb8fb532561279cfd
Tanvir Mahin <55342128+TanvirMahin24@users.noreply.github.com>
2020-05-12 01:38:43 +0600

    task 3 complete

--- Page5/landing5.js
+++ Page5/landing5.js
@@ -4,3 +4,14 @@ new simpleParallax(image, {
     transition: 'cubic-bezier(0,0,0,1)',
     scale: 1.35,
 });
+
+const target = document.querySelector('.mob');
+const toggleOff = document.querySelector('.nav button');
+
+const toggleOn = document.querySelector('.blank button').addEventListener('click', (e) => {
+    target.style.top = '0px';
+});
+
+toggleOff.addEventListener('click', (e) => {
+    target.style.top = '-2000px';
+})
